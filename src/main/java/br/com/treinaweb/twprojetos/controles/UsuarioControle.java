c6f3ef92868c2e04199ffcdd41ca355670c91e92
Cleyson Lima <cleysonph@gmail.com>
2021-04-28 01:26:41 -0300

    08:16 - cria página de perfil do usuario logado

--- src/main/java/br/com/treinaweb/twprojetos/controles/UsuarioControle.java
+++ src/main/java/br/com/treinaweb/twprojetos/controles/UsuarioControle.java
@@ -1,14 +1,34 @@
 package br.com.treinaweb.twprojetos.controles;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.treinaweb.twprojetos.entidades.Funcionario;
+import br.com.treinaweb.twprojetos.repositorios.FuncionarioRepositorio;
 
 @Controller
 public class UsuarioControle {
 
+    @Autowired
+    private FuncionarioRepositorio funcionarioRepositorio;
+
     @GetMapping("/login")
     public String login() {
         return "usuario/login";
+    }
+
+    @GetMapping("/perfil")
+    public ModelAndView perfil(Principal principal) {
+        ModelAndView modelAndView = new ModelAndView("usuario/perfil");
+
+        Funcionario usuario = funcionarioRepositorio.findByEmail(principal.getName()).get();
+        modelAndView.addObject("usuario", usuario);
+
+        return modelAndView;
     }
     
 }
